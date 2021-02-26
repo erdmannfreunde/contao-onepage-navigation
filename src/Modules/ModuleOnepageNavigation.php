c1cc3d402600c83848b611e5905cd1377e303149
Dennis Erdmann <dennis@erdmann-freunde.de>
2021-02-26 18:29:14 +0100

    fix error when there is no article, closes #16

--- src/Modules/ModuleOnepageNavigation.php
+++ src/Modules/ModuleOnepageNavigation.php
@@ -1,13 +1,15 @@
-<?php 
+<?php
 
-/**
- * @package     Onepage Navigation
- * @author      Sascha Brandhoff (https://github.com/Sascha-Brandhoff/onepage-navigation)
- * @author      Basti Buck (http://www.bastibuck.de)
- * @license     LGPLv3
- * @copyright   Erdmann & Freunde (https://erdmann-freunde.de)
+declare(strict_types=1);
+
+/*
+ * Contao Onepage Navigation for Contao Open Source CMS.
+ *
+ * @copyright  Copyright (c) 2021, Erdmann & Freunde
+ * @author     Erdmann & Freunde <https://erdmann-freunde.de>
+ * @license    MIT
+ * @link       http://github.com/erdmannfreunde/contao-onepage-navigation
  */
-
 
 namespace EuF\OnepageNavigation\Modules;
 
@@ -16,98 +18,106 @@ use Patchwork\Utf8;
 class ModuleOnepageNavigation extends \Module
 {
     /**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'mod_onepage_navigation';
-    
+     * Template.
+     *
+     * @var string
+     */
+    protected $strTemplate = 'mod_onepage_navigation';
+
     /**
-     * Display a wildcard in the back end
+     * Display a wildcard in the back end.
      *
      * @return string
      */
-	public function generate()
-	{
-        if (TL_MODE == 'BE')
-		{
-			/** @var BackendTemplate|object $objTemplate */
-			$objTemplate = new \BackendTemplate('be_wildcard');
+    public function generate()
+    {
+        if (TL_MODE === 'BE')
+        {
+            /** @var BackendTemplate|object $objTemplate */
+            $objTemplate = new \BackendTemplate('be_wildcard');
 
-			$objTemplate->wildcard = '### ' . Utf8::strtoupper($GLOBALS['TL_LANG']['FMD']['onepage_navigation'][0]). ' ###';
-			$objTemplate->title = $this->headline;
-			$objTemplate->id = $this->id;
-			$objTemplate->link = $this->name;
-			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+            $objTemplate->wildcard = '### '.Utf8::strtoupper($GLOBALS['TL_LANG']['FMD']['onepage_navigation'][0]).' ###';
+            $objTemplate->title    = $this->headline;
+            $objTemplate->id       = $this->id;
+            $objTemplate->link     = $this->name;
+            $objTemplate->href     = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id='.$this->id;
 
-			return $objTemplate->parse();
-		}		
-		
-		return parent::generate();
-	}
+            return $objTemplate->parse();
+        }
+
+        return parent::generate();
+    }
 
     /**
-	 * Generate the module
-	 */
-	protected function compile()
-	{
+     * Generate the module.
+     */
+    protected function compile()
+    {
         // initialize empty array
-		$arrNavigation = array();
-		
-		// get current page id
-		$intPageID = $GLOBALS['objPage']->id;
+        $arrNavigation = [];
 
-		// override page ID if a rootPage is defined
-		if($this->defineRoot) {
-			$PageID = \PageModel::findById($this->rootPage);
-			$PageAlias = $PageID->getFrontendUrl('');
-			$intPageID = $PageID->id;
-		
-		} else {
-			$PageID    = \PageModel::findById($intPageID);
-			$PageAlias = $PageID->getFrontendUrl('');
-		}
-		
-		// get articles by page id
-		$objArticle = \ArticleModel::findByPid($intPageID, array('order' => 'sorting'));
-		
-		// put articles into array if they should be displayed as navigation items
-		while($objArticle->next())
-		{
-			if($objArticle->addNavigation && $objArticle->published)
-			{
-				if($objArticle->navigation_title)
-				{
-					$objArticle->title = $objArticle->navigation_title;
-				}
+        // get current page id
+        $intPageID = $GLOBALS['objPage']->id;
 
-				// get jumpTo target and add to article so it can be rendered in template
-				$cssID = \StringUtil::deserialize($objArticle->cssID);
+        // override page ID if a rootPage is defined
+        if ($this->defineRoot)
+        {
+            $PageID    = \PageModel::findById($this->rootPage);
+            $PageAlias = $PageID->getFrontendUrl('');
+            $intPageID = $PageID->id;
+        }
+        else
+        {
+            $PageID    = \PageModel::findById($intPageID);
+            $PageAlias = $PageID->getFrontendUrl('');
+        }
 
-				if (empty($cssID[0]))
-				{
-					if($objArticle->navigation_jumpTo) {
-						$cssID = array($objArticle->navigation_jumpTo, $cssID[1]);
-					}
-					else {
-						$cssID = array('article-'.$objArticle->id, $cssID[1]);
-					}
-				}
-				$objArticle->cssID = serialize($cssID);
-				$objArticle->onepage_jump = $PageAlias.'#'.$cssID[0];
+        // get articles by page id
+        $objArticle = \ArticleModel::findByPid($intPageID, ['order' => 'sorting']);
 
-				$arrNavigation[] = (object) $objArticle->row();
-			}
-		}
+        if (null !== $objArticle)
+        {
+            // put articles into array if they should be displayed as navigation items
+            while ($objArticle->next())
+            {
+                if ($objArticle->addNavigation && $objArticle->published)
+                {
+                    if ($objArticle->navigation_title)
+                    {
+                        $objArticle->title = $objArticle->navigation_title;
+                    }
 
-		if($arrNavigation) {
-			// add first and last class to items
-			$arrNavigation[0]->css = 'first';
-			$arrNavigation[count($arrNavigation) - 1]->css = 'last';
-	
-			// send to template
-			$this->Template->hasItems = true;
-			$this->Template->navigation = $arrNavigation;
-		}
+                    // get jumpTo target and add to article so it can be rendered in template
+                    $cssID = \StringUtil::deserialize($objArticle->cssID);
 
-	}
+                    if (empty($cssID[0]))
+                    {
+                        if ($objArticle->navigation_jumpTo)
+                        {
+                            $cssID = [$objArticle->navigation_jumpTo, $cssID[1]];
+                        }
+                        else
+                        {
+                            $cssID = ['article-'.$objArticle->id, $cssID[1]];
+                        }
+                    }
+                    $objArticle->cssID        = serialize($cssID);
+                    $objArticle->onepage_jump = $PageAlias.'#'.$cssID[0];
+
+                    $arrNavigation[] = (object) $objArticle->row();
+                }
+            }
+        }
+
+        if ($arrNavigation)
+        {
+            // add first and last class to items
+            $arrNavigation[0]->css                          = 'first';
+            $arrNavigation[\count($arrNavigation) - 1]->css = 'last';
+
+            // send to template
+            $this->Template->hasItems   = true;
+            $this->Template->navigation = $arrNavigation;
+        }
+    }
 }
