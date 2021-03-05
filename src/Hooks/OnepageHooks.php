7a5cb2319dfb7f2fdf984cec262c32c626d5913c
Dennis Erdmann <dennis@erdmann-freunde.de>
2021-03-05 17:00:58 +0100

    add onepage_article class when jumpto is empty

--- src/Hooks/OnepageHooks.php
+++ src/Hooks/OnepageHooks.php
@@ -1,13 +1,15 @@
-<?php 
+<?php
 
-/**
- * @package     Onepage Navigation
- * @author      Sascha Brandhoff (https://github.com/Sascha-Brandhoff/onepage-navigation)
- * @author      Basti Buck (http://www.bastibuck.de)
- * @license     LGPLv3
- * @copyright   Erdmann & Freunde (https://erdmann-freunde.de)
+declare(strict_types=1);
+
+/*
+ * Contao Onepage Navigation for Contao Open Source CMS.
+ *
+ * @copyright  Copyright (c) 2021, Erdmann & Freunde
+ * @author     Erdmann & Freunde <https://erdmann-freunde.de>
+ * @license    MIT
+ * @link       http://github.com/erdmannfreunde/contao-onepage-navigation
  */
-
 
 namespace EuF\OnepageNavigation\Hooks;
 
@@ -16,32 +18,37 @@ use Contao\StringUtil;
 
 class OnepageHooks extends Controller
 {
-
     public function onGetArticle($row): void
     {
-        if ($row->addNavigation && ($anchor = $row->navigation_jumpTo)) {
+        if ($row->addNavigation)
+        {
             $cssId      = StringUtil::deserialize($row->cssID, true);
-            $cssId[0]   = $anchor;
-            $cssId[1]  .= ' onepage_article';
+            $cssId[1] .= ' onepage_article';
             $row->cssID = serialize($cssId);
+            $anchor     = $row->navigation_jumpTo;
+        }
+
+        if ($row->navigation_jumpTo)
+        {
+            $cssId[0] = $row->navigation_jumpTo;
         }
     }
 
-	public function replaceScrollTag($strTag)
-	{
-        $value = false;
-        $arrExplode = explode("::", $strTag);
+    public function replaceScrollTag($strTag)
+    {
+        $value      = false;
+        $arrExplode = explode('::', $strTag);
 
-		if($arrExplode[0] === "scroll" && count($arrExplode) === 3)
-		{
-			if(substr($arrExplode[1], 0, 1) == "#")
-			{
-				$arrExplode[1] = substr($arrExplode[1], 1);
-			}
+        if ('scroll' === $arrExplode[0] && 3 === \count($arrExplode))
+        {
+            if ('#' === substr($arrExplode[1], 0, 1))
+            {
+                $arrExplode[1] = substr($arrExplode[1], 1);
+            }
 
-			$value = '<a href="'.\Environment::get("requestUri").'#' . $arrExplode[1] . '" data-scroll="' . $arrExplode[1] . '" title="' . $arrExplode[2] . '">' . $arrExplode[2] . '</a>';
-		}
+            $value = '<a href="'.\Environment::get('requestUri').'#'.$arrExplode[1].'" data-scroll="'.$arrExplode[1].'" title="'.$arrExplode[2].'">'.$arrExplode[2].'</a>';
+        }
 
-		return $value;
-	}
+        return $value;
+    }
 }
