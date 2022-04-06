3078c18888cf1fa9ea7bced368e7afe13c856bba
Dennis Erdmann <dennis@erdmann-freunde.de>
2022-04-06 16:28:14 +0200

    add first draft for vanilla js solution

---
+++ src/Resources/public/onepage_navigation.js
@@ -0,0 +1,89 @@
+function Onepage(list, options) {
+    options = options || {};
+    const articles = options.articles || document.querySelectorAll('.mod_article');
+    const pushUrl = options.pushUrl || false;
+    const offset = parseInt(options.offset, 10) || 0;
+
+    const offsetArray = Number(offset) - 100 + '% 0% '+  Number(-offset) + '% 0%';
+    const el = document.querySelectorAll('a[href*="#"]:not([href="#"]):not([href="#0"])');
+    const uri = window.location.href.split("#")[0];
+
+    el.forEach ((anchor) => {
+        anchor.addEventListener('click', (event) => {
+            // check if on-page links
+            if (location.pathname.replace(/^\//, '') == anchor.pathname.replace(/^\//, '') && location.hostname == anchor.hostname) {
+
+                // figure out element to scroll to
+                let target = anchor.hash;
+                let article = document.getElementById(anchor.hash.slice(1)).offsetTop;
+
+                if (target.length) {
+                    // only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+
+                    // scroll to selected article
+                    scrollTo({
+                        top: article,
+                        behavior: "smooth"
+                    });
+
+                    // change url
+                    if (pushUrl) {
+                        history.pushState("", "", uri + anchor.hash);
+                    }
+                };
+            }
+        });
+    });
+
+    // set nav active when scrolling
+    const navActive = (article) => {
+        listItems = list.querySelectorAll('li');
+        let active = list.querySelector('li.active');
+        actualItem = list.querySelector('li[data-onepage-link="'+ article +'"]') ? list.querySelector('li[data-onepage-link="'+ article +'"]') : null;
+
+        if (typeof(active) != 'undefined' && active != null) {
+            active.classList.remove('active');
+        }
+
+        if (actualItem) {
+            actualItem.classList.add('active');
+        } else {
+            article = getPreviousSibling(document.querySelector('#'+ article), '.onepage_article');
+            actualItem = list.querySelector('li[data-onepage-link="'+ article.id +'"]');
+            actualItem.classList.add('active');
+        }
+    };
+
+    let getPreviousSibling = function (elem, selector) {
+        var sibling = elem.previousElementSibling;
+
+        // if no previous element exists with this selector, use current element
+        if (!sibling) return elem.nextElementSibling;
+        
+        // if selector does not exist, return previous element
+        if (!selector) return sibling;
+
+        while (sibling) {
+            if (sibling.matches(selector)) return sibling;
+            sibling = sibling.previousElementSibling;
+        }
+    };
+
+
+    const articleObserver = new IntersectionObserver (function (entries, observer) {
+        entries.forEach(function(entry) {
+
+            if (entry.isIntersecting) {
+                navActive(entry.target.id);
+                //entry.target.classList.add("article--active");
+            } else {
+                //entry.target.classList.remove("article--active");
+            }
+        });
+    }, { rootMargin: offsetArray });
+
+    articles.forEach ((article) => {
+        articleObserver.observe(article);
+    });
+};
